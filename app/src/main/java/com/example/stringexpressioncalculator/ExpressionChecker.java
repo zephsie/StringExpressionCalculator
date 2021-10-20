00723e6a3adf71933b9937b0c68fa60ec8dd2f39
zephsie <90218535+zephsie@users.noreply.github.com>
2021-10-20 21:41:36 +0300

    20 10 2021

---
+++ app/src/main/java/com/example/stringexpressioncalculator/ExpressionChecker.java
@@ -0,0 +1,91 @@
+package com.example.stringexpressioncalculator;
+
+import android.text.Html;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExpressionChecker implements IExpressionChecker {
+    public boolean check(List<String> inputItems, String currentItem) {
+        if (!inputItems.isEmpty()) {
+            currentItem = currentItem
+                    .replace("e", "" + Math.E)
+                    .replace(Html.fromHtml("&#960;"), "" + Math.PI)
+                    .replace(",", ".");
+
+            boolean isEOrPI = false;
+
+            if (inputItems.get(inputItems.size() - 1).equals("e")
+                    || inputItems.get(inputItems.size() - 1)
+                    .replace(Html.fromHtml("&#960;"), "pi").equals("pi")) isEOrPI = true;
+
+            StringBuilder items0 = new StringBuilder();
+
+            for (String inputItem : inputItems) {
+                items0.append(inputItem);
+            }
+
+            String expression = items0.toString()
+                    .replace("e", "" + Math.E)
+                    .replace(Html.fromHtml("&#960;"), "" + Math.PI)
+                    .replace(",", ".")
+                    .replaceFirst("^-", "0-")
+                    .replace("(-", "(0-");
+
+            ArrayList<String> items = new ArrayList<>();
+
+            String regex = "([0-9]+[.]$)|([0-9]*[.]?[0-9]+)|([0-9]+[.]?[0-9]*)|[+\\-*^/()]";
+
+            Pattern pattern = Pattern.compile(regex);
+
+            Matcher matcher = pattern.matcher(expression);
+
+            int bktCount = 0;
+
+            while (matcher.find()) {
+                items.add(matcher.group());
+
+                if (matcher.group().equals("(")) bktCount++;
+                if (matcher.group().equals(")")) bktCount--;
+            }
+
+            String lastItem = items.get(items.size() - 1);
+
+            switch (lastItem) {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "^": {
+                    if (currentItem.matches("[.+\\-*^/)]")) return false;
+                    break;
+                }
+                case "(": {
+                    if (currentItem.matches("[.+*^/)]")) return false;
+                    break;
+                }
+                case ")": {
+                    if (currentItem.matches("[0-9.(]")) return false;
+                    break;
+                }
+                case "0": {
+                    if (currentItem.matches("[0-9]")) return false;
+                    break;
+                }
+                default: {
+                    if (currentItem.length() > 1 && currentItem.contains(".")) return false;
+                    if (lastItem.contains(".") && currentItem.contains(".")) return false;
+                    if (lastItem.endsWith(".") && !currentItem.matches("[0-9]")) return false;
+                    if (currentItem.equals("(")) return false;
+                    if (isEOrPI && currentItem.matches("[0-9]")) return false;
+                }
+            }
+
+            return bktCount != 0 || !currentItem.equals(")");
+        } else {
+            return !currentItem.matches("[.+*^/)]");
+        }
+    }
+}
