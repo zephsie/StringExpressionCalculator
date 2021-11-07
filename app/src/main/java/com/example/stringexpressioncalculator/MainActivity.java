6bab15cc6e3cb688c5af546b46dd3e07c662852d
zephsie <90218535+zephsie@users.noreply.github.com>
2021-11-07 19:23:36 +0300

    07 11 2021 N2

--- app/src/main/java/com/example/stringexpressioncalculator/MainActivity.java
+++ app/src/main/java/com/example/stringexpressioncalculator/MainActivity.java
@@ -36,9 +36,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
-        onWindowFocusChanged(true);
+        setContentView(R.layout.activity_main);
 
         if (Build.VERSION.SDK_INT >= 28) {
             getWindow().getAttributes().layoutInDisplayCutoutMode =
